5454695d520f3186bbbf066c002a92fb140332d3
Luka Tskhvaradze <130844113+Cxvarisorka@users.noreply.github.com>
2024-02-27 13:21:38 +0400

    Variable Creating Rules

--- starter/script.js
+++ starter/script.js
@@ -1,5 +1,39 @@
-console.log("Hello");
+// console.log("Hello");
 
-let firstName = "Luka";
+// let firstName = "Luka";
 
-console.log(firstName);
+// console.log(firstName);
+
+// You cant use any symbol except $ _ 
+// You cant create variable with starting numbers in name
+
+// let 3years = 3; error syntax
+// let luka&mari = 17; 
+
+// You cant use reserved keywords for variable names
+// like: let, function, const and so on
+
+// let function = 10; this will result error
+
+// Also there is rule that we dont use uppercase starting variables
+// We are using that kind of rule for something else
+
+// let Person = "Luka"; // This is not illegal but dont use it.
+
+// Also if you want to create const variable you can use Uppercase
+
+// const keyword creates variable wich value will not change
+// const PI = 3.14;
+
+// its good practice to give variables descriptive names
+// descriptive names is names wich describes what variable is storing
+
+// examples
+
+// Good practice, descriptive variable names.
+// let myFirstJob = "Programmer";
+// let myCurrentJob = "Teacher";
+
+// Bad practice, not descriptive variable names.
+// let job1 = "Programmer";
+// let job2 = "Teacher";
